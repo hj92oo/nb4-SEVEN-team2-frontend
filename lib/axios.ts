021969e40511672eabebe0e8eb01d0a1de2f70df
shwon <kairise1234@gmail.com>
2025-08-28 15:48:44 +0900

    fix: url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,12 +1,21 @@
 import * as Axios from 'axios';
 
+// 환경 변수가 없는 경우 명확히 에러
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+if (!BASE_URL) {
+  throw new Error('NEXT_PUBLIC_API_URL is not defined');
+}
 
 export const axios = Axios.default.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL, // SSR에서도 절대 URL 사용
 });
 
+// 요청 인터셉터로 로그 확인
 axios.interceptors.request.use((config) => {
-  console.log(`[프론트] ${config.method?.toUpperCase()} ${config.url}`);
+  console.log(
+    `[프론트 SSR] ${config.method?.toUpperCase()} ${config.baseURL}${
+      config.url
+    }`
+  );
   return config;
 });
