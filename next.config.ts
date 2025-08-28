4fd9d918ec2e298e2c4222ed41241c3f8473227b
hykim012 <scfe97@naver.com>
2025-08-28 15:38:16 +0900

    fix the next.cofig.ts

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,16 @@ const nextConfig: NextConfig = {
         hostname: 'localhost',
         port: '3001',
       },
-      
+     {
+        protocol: 'https',
+        hostname: 'https://nb4-seven-team2-backend.onrender.com/',
+      },
+      {
+        protocol: 'http', // 또는 'https' 사용
+        hostname: 'nb4-seven-team2-backend-fgxe.onrender.com',
+        port: '', // 포트 없으면 빈 문자열
+        pathname: '/uploads/**', // 경로 패턴, 임시
+      }
       /*{
         protocol: 'https',
         hostname: 'sprint-be-project.s3.ap-northeast-2.amazonaws.com',
