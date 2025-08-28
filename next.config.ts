6cebb3e8c708ea8d2557dfc1f0efc3b769f27dfc
shwon <kairise1234@gmail.com>
2025-08-28 15:31:57 +0900

    fix: next.config.ts의 이미지 허용 수정

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'sprint-be-project.s3.ap-northeast-2.amazonaws.com',
       },
+      {
+        protocol: 'http', // 또는 'https' 사용
+        hostname: 'nb4-seven-team2-backend-fgxe.onrender.com',
+        port: '', // 포트 없으면 빈 문자열
+        pathname: '/uploads/**', // 경로 패턴, 임시
+      },
     ],
   },
 };
