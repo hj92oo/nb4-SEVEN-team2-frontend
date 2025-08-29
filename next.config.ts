96e15345be181443ccf9879965c60b10af785690
hykim012 <scfe97@naver.com>
2025-08-29 11:08:26 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,15 +22,17 @@ const nextConfig: NextConfig = {
         hostname: 'nb4-seven-team2-backend.onrender.com',
       },
       {
-        protocol: 'http', // 또는 'https' 사용
+        protocol: 'http',
         hostname: 'nb4-seven-team2-backend.onrender.com',
         port: '', // 포트 없으면 빈 문자열
         pathname: '/uploads/**', // 경로 패턴, 임시
       },
-      /*{
+      {
         protocol: 'https',
-        hostname: 'sprint-be-project.s3.ap-northeast-2.amazonaws.com',
-      }, 수정 이미지 오류*/
+        hostname: 'nb4-seven-team2-backend.onrender.com',
+        port: '', // 포트 없으면 빈 문자열
+        pathname: '/uploads/**', // 경로 패턴, 임시
+      }
 
     ],
   },
